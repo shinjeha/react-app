476a767edbf62ff93f6d7bdcda7abc27db188f69
shinjeha1993 <shinjeha1993@gmail.com>
2021-02-12 23:09:39 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Subject extends Component {
-  render() { // class안에 소속된 함수는 function 생략
-    return (
-      <header>
-        <h1>WEB</h1>
-        world wide web!
-      </header>
-    );
-  }
-}
-
 class TOC extends Component {
   render() {
     return (
@@ -30,9 +19,20 @@ class Content extends Component {
   render() {
     return (
       <article>
-        <h2>HTML</h2>
-        HTML is HypeText Markup Language.
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
       </article>
+    );
+  }
+}
+
+class Subject extends Component {
+  render() { // class안에 소속된 함수는 function 생략
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
+      </header>
     );
   }
 }
@@ -41,9 +41,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject title="WEB" sub="world wide web"></Subject>
+        <Subject title="React" sub="For UI"></Subject>
         <TOC></TOC>
-        <Content></Content>
+        <Content title="HTML" desc="HTML is..."></Content>
       </div>
     );
   }
